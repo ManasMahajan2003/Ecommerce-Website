d5dbf927521170db2ff6f732d4b28d3fda5e9dec
Manas <manasmahajan2003@yahoo.com>
2024-07-14 11:35:32 +0530

    Initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+import flowbitePlugin from "flowbite/plugin";
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbitePlugin],
+}
